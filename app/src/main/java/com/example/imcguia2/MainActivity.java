764528c77c28bbb1d9e96a7e840e9a29c3c80ac7
Yasmin <yasmin.torres@o365.unab.edu.co>
2021-11-23 09:31:04 -0500

    Se agrego el try-catch calcular

--- app/src/main/java/com/example/imcguia2/MainActivity.java
+++ app/src/main/java/com/example/imcguia2/MainActivity.java
@@ -45,64 +45,67 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                peso = Double.parseDouble(edtPeso.getText().toString());
-                estatura = Double.parseDouble(edtEstatura.getText().toString());
-                imc = peso/(estatura*estatura);
-                txtImc.setText("IMC:  "+redondear(imc));
-                //borrar();
-                //validacion para la categoria
-                if(imc<18.5){
-                    txtCategoria.setBackgroundColor(Color.parseColor("#728FCE"));
-                    txtCategoria.setText("Bajo Peso");
-                }if(imc>=18.5 && imc<25){
-                    txtCategoria.setBackgroundColor(Color.parseColor("#4CC417"));
-                    txtCategoria.setText("Peso Normal");
-                }if(imc>=25 && imc<30){
-                    txtCategoria.setBackgroundColor(Color.parseColor("#FFFF00"));
-                    txtCategoria.setText("Sobrepeso");
-                }if(imc>=30 && imc<35){
-                    txtCategoria.setBackgroundColor(Color.parseColor("#E9AB17"));
-                    txtCategoria.setText("Obesidad I");
-                }if(imc>=35 && imc<40){
-                    txtCategoria.setBackgroundColor(Color.parseColor("#FF4500"));
-                    txtCategoria.setText("Obesidad II");
-                }if(imc>=40) {
-                    txtCategoria.setBackgroundColor(Color.parseColor("#FF0000"));
-                    txtCategoria.setText("Obesidad III");
-                }
+                try{
+                    peso = Double.parseDouble(edtPeso.getText().toString());
+                    estatura = Double.parseDouble(edtEstatura.getText().toString());
+                    imc = peso/(estatura*estatura);
+                    txtImc.setText("IMC:  "+redondear(imc));
+                    //borrar();
+                    //validacion para la categoria
+                    if(imc<18.5){
+                        txtCategoria.setBackgroundColor(Color.parseColor("#728FCE"));
+                        txtCategoria.setText("Bajo Peso");
+                    }if(imc>=18.5 && imc<25){
+                        txtCategoria.setBackgroundColor(Color.parseColor("#4CC417"));
+                        txtCategoria.setText("Peso Normal");
+                    }if(imc>=25 && imc<30){
+                        txtCategoria.setBackgroundColor(Color.parseColor("#FFFF00"));
+                        txtCategoria.setText("Sobrepeso");
+                    }if(imc>=30 && imc<35){
+                        txtCategoria.setBackgroundColor(Color.parseColor("#E9AB17"));
+                        txtCategoria.setText("Obesidad I");
+                    }if(imc>=35 && imc<40){
+                        txtCategoria.setBackgroundColor(Color.parseColor("#FF4500"));
+                        txtCategoria.setText("Obesidad II");
+                    }if(imc>=40) {
+                        txtCategoria.setBackgroundColor(Color.parseColor("#FF0000"));
+                        txtCategoria.setText("Obesidad III");
+                    }
 
-                //validacion peso ideal femenino
-                if (estatura>1.45 && btnFemenino.isChecked()){
-                    txtIdeal.setText("45.0 Kg-52.0 Kg");
-                }if (estatura>1.52 && btnFemenino.isChecked()){
-                    txtIdeal.setText("46.0 Kg-54.0 Kg");
-                }if (estatura>1.60 && btnFemenino.isChecked()){
-                    txtIdeal.setText("50.0 Kg-60.0 Kg");
-                }if (estatura>1.65 && btnFemenino.isChecked()){
-                    txtIdeal.setText("53.0 Kg-63.0 Kg");
-                }if (estatura>1.70 && btnFemenino.isChecked()){
-                    txtIdeal.setText("58.0 Kg-67.0 Kg");
-                }if (estatura>1.75 && btnFemenino.isChecked()){
-                    txtIdeal.setText("61.0 Kg-72.0 Kg");
-                }
+                    //validacion peso ideal femenino
+                    if (estatura>1.45 && btnFemenino.isChecked()){
+                        txtIdeal.setText("45.0 Kg-52.0 Kg");
+                    }if (estatura>1.52 && btnFemenino.isChecked()){
+                        txtIdeal.setText("46.0 Kg-54.0 Kg");
+                    }if (estatura>1.60 && btnFemenino.isChecked()){
+                        txtIdeal.setText("50.0 Kg-60.0 Kg");
+                    }if (estatura>1.65 && btnFemenino.isChecked()){
+                        txtIdeal.setText("53.0 Kg-63.0 Kg");
+                    }if (estatura>1.70 && btnFemenino.isChecked()){
+                        txtIdeal.setText("58.0 Kg-67.0 Kg");
+                    }if (estatura>1.75 && btnFemenino.isChecked()){
+                        txtIdeal.setText("61.0 Kg-72.0 Kg");
+                    }
 
-                //validacion peso ideal masculino
-                if (estatura>1.5 && btnMasculino.isChecked()){
-                    txtIdeal.setText("45.0 Kg-56.0 Kg");
-                }if (estatura>1.55 && btnMasculino.isChecked()){
-                    txtIdeal.setText("53.0 Kg-59.0 Kg");
-                }if (estatura>1.60 && btnMasculino.isChecked()){
-                    txtIdeal.setText("51.0 Kg-64.0 Kg");
-                }if (estatura>1.65 && btnMasculino.isChecked()){
-                    txtIdeal.setText("54.0 Kg-68.0 Kg");
-                }if (estatura>1.70 && btnMasculino.isChecked()){
-                    txtIdeal.setText("58.0 Kg-72.0 Kg");
-                }if (estatura>1.75 && btnMasculino.isChecked()){
-                    txtIdeal.setText("61.0 Kg-76.0 Kg");
-                }if (estatura>1.80 && btnMasculino.isChecked()){
-                    txtIdeal.setText("65.0 Kg-81.0 Kg");
+                    //validacion peso ideal masculino
+                    if (estatura>1.5 && btnMasculino.isChecked()){
+                        txtIdeal.setText("45.0 Kg-56.0 Kg");
+                    }if (estatura>1.55 && btnMasculino.isChecked()){
+                        txtIdeal.setText("53.0 Kg-59.0 Kg");
+                    }if (estatura>1.60 && btnMasculino.isChecked()){
+                        txtIdeal.setText("51.0 Kg-64.0 Kg");
+                    }if (estatura>1.65 && btnMasculino.isChecked()){
+                        txtIdeal.setText("54.0 Kg-68.0 Kg");
+                    }if (estatura>1.70 && btnMasculino.isChecked()){
+                        txtIdeal.setText("58.0 Kg-72.0 Kg");
+                    }if (estatura>1.75 && btnMasculino.isChecked()){
+                        txtIdeal.setText("61.0 Kg-76.0 Kg");
+                    }if (estatura>1.80 && btnMasculino.isChecked()){
+                        txtIdeal.setText("65.0 Kg-81.0 Kg");
+                    }
+                } catch (NumberFormatException numberFormatException) {
+                    numberFormatException.printStackTrace();
                 }
-
 
             }
         });
